bd4491fc0de75cdc53cc37418e855ea77dfe7fdc
keremersann <keremersan01@gmail.com>
2023-03-03 01:26:14 +0300

    Profile page is designed

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,68 @@
-import "./profile.scss"
+import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { currentUser } = useContext(AuthContext);
 
-export default Profile
+  return (
+    <div className="profile">
+      <div className="images">
+        <img src={currentUser.profilePic} alt="" className="cover" />
+        <img src={currentUser.profilePic} alt="" className="profilePic" />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="www.facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="www.instagram.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="www.twitter.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="www.linkedin.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="www.pinterest.com">
+              <PinterestIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Kerem Ersan</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>kerem.ersan</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
